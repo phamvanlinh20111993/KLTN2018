7f1fb0268af50c24f545e376846904ae3da81b3b
phamvanlinh20111993 <phamvanlinh20111993@gmail.com>
2018-05-27 01:24:01 +0700

    thu 7 chan, hoai niem va code, mai se lam cai khac

--- public/js/lib/comparestring.js
+++ public/js/lib/comparestring.js
@@ -1,5 +1,10 @@
 		
-	//chuan hoa chuoi
+    /**
+      *
+      * @description: chuan hoa chuoi
+      * @param s1: String s1
+      *
+     **/
 	function standardizeString(s) {
         // change string to lowercase
         var lower = s.toLowerCase();
@@ -24,33 +29,103 @@
             }
             index++;
         }
+
         return lower;
     }
 
-    /**
-     * @description khoi phuc lai chuoi tro ve ban dau
-	 * @origin la chuoi ban dau chua chuan hoa
-	 * @replace la mot mang cac tu da chuan hoa cac ki tu
-	 *	
+    
+    /** 
+     *
+     * @description tao lai chuoi phan biet them sua xoa
+     * @param splitstring  la chuỗi đã bị cắt bởi dấu  " ", sẽ chứa 1 mảng các từ
+     * @param vitri là mảng chứa các từ trùng nhau giữa s1 và s2
+     * @param status, 0 to mau la do khi xoa, 1 to mau la xanh khi them
+     *
     **/
-    function recoverString(origin, replace){
+    function rewriteString(splitstring, vitri){
 
-    	//doi chuoi tu mang thanh chuoi string
-    	var ostring = ""
-    	for(var ind = 0; ind < replace.length; ind++)
-    		ostring += replace[ind] + " "
+    	var ptagdelete = "<span style='color:red;'>", endtag = "</span>"
+    	if(status = 1)
+			ptagadd = "<span style='color:blue;'>"
 
-    	origin = origin.trim()
-    	replace = replace.trim()
-    	// for(var ind = 0; ind < origin.length; ind++){
-    	// 	if(origin[ind] == " ") continue
-    	// 	else{
-    	// 		ostring[ind] = ""
-    	// 	}
-    	// }
+		//chay kiem tra chuoi
+		var strOld = "", position = 0
+		for(var ind = 0; ind < vitri.length; ind++){
+			for(var pos = position; pos < splitstring.length; pos++){
+				if(splitstring[pos] == vitri[ind]){
+					strOld += splitstring[pos]+ " ";
+					position = pos+1;
+					break;
+				}else{
+					strOld += ptagdelete +  splitstring[pos] + endtag + " ";
+				}
+			}
+		}
+		
+		for(var pos = position; pos < splitstring.length; pos++){
+			strOld += ptagdelete +  splitstring[pos] + endtag + " ";
+		}
+
+		return strOld.trim();
     }
 
-    //kiem tra so sanh 2 chuoi
+    /**
+      * @description: khoi phuc chuoi ve dang ban dau
+      * @param origin chuoi ban dau chua chuan hoa
+      * @param newstring - object chuoi da chuan hoa, vi tri tu trung nhau
+      *
+    **/
+    function recoverString(origin, newstring) {
+    	// body...
+    	var arrorigin = origin.split(" "), ind = 0, index = 0, 
+    		pos = 0, recoverstr = {}, mark = true, position = 0;
+    	while(true){
+    		if(ind == arrorigin.length)
+    			break;
+    		//bo ki tu trang
+    		if(arrorigin[ind] == ""){
+    			arrorigin.splice(ind, 1)
+    			ind--
+    		}
+    		ind++;
+    	}
+
+    	recoverstr.str = [], recoverstr.pos = []
+    	//khoi phuc chuoi
+    	for(ind = 0; ind < newstring.str.length; ind++){
+    		if(arrorigin[ind].toLowerCase() == newstring.str[ind].toLowerCase()){
+			    recoverstr.str[ind] = arrorigin[ind]
+    		}else{
+    			mark = false;
+    			break;
+    		}
+    	}
+
+    	//khoi phuc vi tri trung nhau
+    	for(ind = 0; ind < newstring.pos.length; ind++){
+    		for(index = pos; index < arrorigin.length; index++){
+    			if(newstring.pos[ind].toLowerCase() == arrorigin[index].toLowerCase()){
+    				recoverstr.pos[position] = arrorigin[index]
+    				position++;
+    				pos = index + 1;
+    				break;
+    			}
+    		}
+    	}
+
+    	if(!mark)
+    		return newstring
+    	else 
+    		return recoverstr
+    }
+
+    /**
+      *
+      * @description: kiem tra so sanh 2 chuoi tim vị tri trung nhau, vi tri da xoa va them vao 
+      * @param s1: String s1
+      * @param s2: String s2
+      *
+     **/
     function doCheck(s1, s2){
 
 		var ostring = s1
@@ -88,63 +163,47 @@
 			
 			if(max < lonnhat){
 				max = lonnhat
+				//mang vi tri luu tru cac tu trung nhau o ca 2 mang
 				for(var index = 0; index < lonnhat; index++)
 					vitri[index] = tamvitri[index]
 			}
 			if(chutrungnhau.length == lonnhat) break;
 		}
 		
-		var ptagdelete = "<span style='color:red;'>", endtag = "</span>",
-			ptagadd = "<span style='color:blue;'>"
-		//chay kiem tra chuoi
-		var strOld = "",strNew = "", position = 0
-		for(var ind = 0; ind < vitri.length; ind++){
-			for(var pos = position; pos < splitostring.length; pos++){
-				if(splitostring[pos] == vitri[ind]){
-					strOld += splitostring[pos]+ " ";
-					position = pos+1;
-					break;
-				}else{
-					strOld += ptagdelete +  splitostring[pos] + endtag + " ";
-				}
-			}
-		}
-		
-		for(var pos = position; pos < splitostring.length; pos++){
-			strOld += ptagdelete +  splitostring[pos] + endtag + " ";
-		}
-		
-		position = 0
-		for(var ind = 0; ind < vitri.length; ind++){
-			for(var pos = position; pos < splitnstring.length; pos++){
-				if(splitnstring[pos] == vitri[ind]){
-					strNew += splitnstring[pos] + " "
-					position = pos+1;
-					break;
-				}else{
-					strNew += ptagadd +  splitnstring[pos] + endtag + " ";
-				}
-			}
-		}
-		
-		for(var pos = position; pos < splitnstring.length; pos++){
-			strNew += ptagadd +  splitnstring[pos] + endtag + " ";
-		}
-		
 		//document.getElementById("kq1").innerHTML = strOld.trim()
 	//	document.getElementById("kq2").innerHTML = strNew.trim()
+		var oldrecover = recoverString(origin, {
+			str: splitostring,
+			pos: vitri
+		})
 
-		var stringReturn = {
-			add: strOld.trim(),
-            del: strNew.trim()
+		var newrecover = recoverString(newstring, {
+			str: splitnstring,
+			pos: vitri
+		})
+
+		return  {
+			add: rewriteString(oldrecover.str, oldrecover.pos, 0),
+            del: rewriteString(newrecover.str, newrecover.pos, 1)
 		}
-
-		return stringReturn
 	}
 
 	//thuc hien nhiem vu
+	var origin = "", newstring = ""
+
+    /**
+      *
+      * @description: tra ve chuoi da chuan hoa va kiem tra trung nhau
+      * @param s1: String s1
+      * @param s2: String s2
+      *
+     **/
 	function doTask(s1, s2){
+
+		origin = s1
+		newstring = s2
 		var contentcopy = standardizeString(s1);
 		var contentupdatecopy = standardizeString(s2);
+
 		return doCheck(contentcopy, contentupdatecopy)
 	}
